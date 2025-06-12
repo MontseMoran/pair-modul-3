67154b3127a12f690d30b250927cc01287adaa19
MontseMoran <m-moran@hotmail.es>
2025-06-12 21:12:06 +0200

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Sara + Montse</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 5)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Visitanos para saber más 
+      </p>
+      <input type="text" placeholder='Pon aquí tu nombre' />
+    </>
+  )
+}
+
+export default App
